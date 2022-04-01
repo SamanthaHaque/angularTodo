42703a365de6d5086f080cd48d62ab3aa7ec1e8b
samanthahaque <samantha.simona.xyz.007@gmail.com>
2022-04-01 13:47:56 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import { ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularCrud13';
+  displayedColumns: string[] = ['productName', 'category','date','freshness' , 'price','comment','action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+
+  constructor(private dialog : MatDialog,private api : ApiService){
+
+  }
+ 
+  ngOnInit(): void {
+    this.getAllProduct();
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent,{
+      width: '30%'
+    }).afterClosed().subscribe(val=>{
+      if(val === 'saved'){
+        this.getAllProduct();
+      }
+    })
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(`Dialog result: ${result}`);
+    // });
+  }
+  getAllProduct(){
+    this.api.getProduct()
+    .subscribe({
+      next:(res)=>{
+       this.dataSource = new MatTableDataSource(res);
+       this.dataSource.paginator = this.paginator;
+       this.dataSource.sort = this.sort;
+      },
+      error:(err)=>{
+        alert('error while fetching the records');
+      }
+    })
+  }
+  editProduct(row:any){
+    this.dialog.open(DialogComponent,{
+      width: '30%',
+      data:row
+    }).afterClosed().subscribe(val=>{
+      if(val === "updated"){
+        this.getAllProduct();
+      }
+    })
+  }
+  deleteProduct(id:any){
+    this.api.deleteProduct(id)
+    .subscribe({
+      next:(res)=>{
+         alert("product deleted successfully");
+         this.getAllProduct();
+      },
+      error:()=>{
+        alert("error occurred while deleting");
+      }
+    })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
